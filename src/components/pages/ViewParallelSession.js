5e2752886ac5cdd5b90e5008c88653f0611816f7
NithyaYamsinghe <nitheeromeshi@gmail.com>
2020-09-29 21:22:54 +0530

    fourth commit

---
+++ src/components/pages/ViewParallelSession.js
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+import ViewParallelSessionScreen from "./../parallelSessions/ViewParallelSessionScreen";
+
+class ViewParallelSession extends Component {
+  render() {
+    return <ViewParallelSessionScreen />;
+  }
+}
+
+export default ViewParallelSession;
